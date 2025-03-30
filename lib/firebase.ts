5cdc36d0403aa7d7d21b689128520312148942b8
Adam-Hrzeta <112137532+Adam-Hrzeta@users.noreply.github.com>
2025-03-30 16:58:34 -0600

    ACTUALIZACIONES

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getAuth, Auth } from "firebase/auth";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCM-edYKqmH3ZJVYGbD0uEColY9d4LmTIo",
@@ -14,7 +17,30 @@ const firebaseConfig = {
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+// Configurar autenticación
+let auth: Auth;
+if (Platform.OS === 'web') {
+  auth = getAuth(app);
+} else {
+  auth = initializeAuth(app, {
+    persistence: ReactNativeAsyncStorage ? 
+      require('firebase/auth').getReactNativePersistence(ReactNativeAsyncStorage) : 
+      require('firebase/auth').inMemoryPersistence
+  });
+}
+
 const db = getFirestore(app);
+
+// Manejar sesión en SecureStore para móviles
+auth.onAuthStateChanged(async (user: any) => {
+  if (Platform.OS !== "web") {
+    if (user) {
+      await SecureStore.setItemAsync("userToken", JSON.stringify(user));
+    } else {
+      await SecureStore.deleteItemAsync("userToken");
+    }
+  }
+});
 
 export { auth, db };
